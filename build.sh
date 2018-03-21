88fb5a34286238b6d3e772b5f56384be2a1ddf7e
geiti94 <geiti94@me.com>
2018-03-21 22:07:48 +0100

    V1.0.1 Prerelease changes

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 export MODEL=star2lte
 export VARIANT=eur
 export ARCH=arm64
-export BUILD_CROSS_COMPILE=/home/tkkg1994/android/omni/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export BUILD_CROSS_COMPILE=/home/geiti94/Android/Toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 RDIR=$(pwd)
